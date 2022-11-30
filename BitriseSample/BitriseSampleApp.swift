ab61105b06444a1bb4e2ceec23119d9d604ff805
mht-naoki-muramoto <n.muramoto@mht-code.com>
2022-11-30 14:14:20 +0900

    add project

---
+++ BitriseSample/BitriseSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseSampleApp.swift
+//  BitriseSample
+//
+//  Created by Naoki Muramoto on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct BitriseSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
